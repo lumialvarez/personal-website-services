5c01823572925c6bf64db10e7acc2eeea859501f
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-05-03 22:50:47 -0500

    Ajuste Cors

--- src/main/java/com/lmalvarez/services/PersonalWebsiteServicesApplication.java
+++ src/main/java/com/lmalvarez/services/PersonalWebsiteServicesApplication.java
@@ -2,19 +2,25 @@ package com.lmalvarez.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class PersonalWebsiteServicesApplication extends SpringBootServletInitializer {
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(PersonalWebsiteServicesApplication.class);
-    }
+public class PersonalWebsiteServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalWebsiteServicesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 }
