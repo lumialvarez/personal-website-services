e1c1d6fad7af4cbe86ea37cf41980aba5c0c8987
Luis Miguel Alvarez <lumi.aalvarez@gmail.com>
2021-05-23 16:14:16 -0500

    Adicion de categoria conocimiento external

---
+++ src/main/java/com/lmalvarez/services/external/CategoriaConocimientoControllerExt.java
@@ -0,0 +1,24 @@
+package com.lmalvarez.services.external;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lmalvarez.services.categoriaConocimiento.CategoriaConocimiento;
+import com.lmalvarez.services.categoriaConocimiento.CategoriaConocimientoService;
+
+@RestController
+@RequestMapping(path = "api/ext/v1/categoria_conocimiento")
+public class CategoriaConocimientoControllerExt {
+	@Autowired
+	private CategoriaConocimientoService categoriaConocimientoService;
+
+	@GetMapping
+	public List<CategoriaConocimiento> getCategoriaConocimientos() {
+		return categoriaConocimientoService.getCategoriaConocimientos();
+	}
+
+}
